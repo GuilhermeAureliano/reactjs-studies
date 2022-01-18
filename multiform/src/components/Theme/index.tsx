8fe2deb6e3481bbf798ebcdd0e4993385cabf56e
Guilherme Aureliano <49173189+GuilhermeAureliano@users.noreply.github.com>
2022-01-18 14:37:06 -0300

    last feature: step 3

--- multiform/src/components/Theme/index.tsx
+++ multiform/src/components/Theme/index.tsx
@@ -2,12 +2,17 @@ import { ReactNode } from 'react';
 
 import * as C from './styles';
 import { Header } from '../Header';
+import { SiderbarItem } from '../../components/SidebarItem';
+import { useForm } from '../../contexts/FormContext';
 
 type Props = {
     children: ReactNode
 }
 
 export const Theme = ({children}: Props) => {
+    
+    const { state } = useForm();
+
     return (
         <C.Container>
             <C.Area>
@@ -15,7 +20,29 @@ export const Theme = ({children}: Props) => {
 
                 <C.Steps>
                     <C.Sidebar>
-                        ...
+                        <SiderbarItem 
+                            title="Pessoal"
+                            description="Se identifique"
+                            icon="profile"
+                            path="/"
+                            active={state.currentStep === 1}
+                        />
+
+                        <SiderbarItem 
+                            title="Profissional"
+                            description="Seu nível"
+                            icon="book"
+                            path="/step2"
+                            active={state.currentStep === 2}
+                        />
+
+                        <SiderbarItem 
+                            title="Contatos"
+                            description="Como te achar"
+                            icon="mail"
+                            path="/step3"
+                            active={state.currentStep === 3}
+                        />
                     </C.Sidebar>
                     
                     <C.Page>
--- multiform/src/pages/FormStep3/index.tsx
+++ multiform/src/pages/FormStep3/index.tsx
@@ -1,9 +1,79 @@
+import { ChangeEvent, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+
 import * as C from './styles';
 
+import { useForm, FormActions } from '../../contexts/FormContext';
+import { Theme } from '../../components/Theme';
+
 export const FormStep3 = () => {
+
+    const history = useNavigate();
+    const { state, dispatch } = useForm();
+
+    useEffect(() => {
+        if (state.name === '') {
+            history('/'); // Volta pra tela inicial
+        } else {
+            dispatch({
+                type: FormActions.setCurrentStep,
+                payload: 3 // Atualizar o passo atual
+            });
+        }
+    }, []);
+
+    const handleNextStep = () => {
+        if (state.email !== '' && state.github !== '') {
+            console.log(state);
+        } else {
+            alert("Preencha os dados!")
+        }
+    }
+
+    const handleEmailChange = (evt: ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: FormActions.setEmail,
+            payload: evt.target.value
+        });
+    }
+
+    const handleGithubChange = (evt: ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: FormActions.setGithub,
+            payload: evt.target.value
+        });
+    }
+
     return (
-        <C.Container>
-            Passo 3
-        </C.Container>
+        <Theme>
+            <C.Container>
+                <p>Passo 3/3</p>
+                <h1>Legal {state.name}, onde te achamos?</h1>
+                <p>Preencha com os seus contatos.</p>
+                <hr/>
+                <label>
+                    Qual seu email?
+                    <input 
+                        type='email'
+                        value={state.email}
+                        onChange={handleEmailChange}
+                    />
+                </label>
+
+                <label>
+                    Qual seu github?
+                    <input 
+                        type='url'
+                        value={state.github}
+                        onChange={handleGithubChange}
+                    />
+                </label>
+
+                <C.Button>
+                    <Link to="/step2" className="backButton">Voltar</Link>
+                    <button onClick={handleNextStep}>Finalizar cadastro</button>
+                </C.Button>
+            </C.Container>
+        </Theme>
     );
 }
